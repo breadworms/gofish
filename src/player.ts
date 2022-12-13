89154663122b263943875ba28444b1ed0dd62cb7
breadworms <breadwormsbusiness@gmail.com>
2022-12-13 00:13:47 +0000

    New maps update

--- src/player.ts
+++ src/player.ts
@@ -21,8 +21,28 @@ function save(player: Player): void {
 }
 
 function load(): Player {
-  return Object.assign(
+  const player = Object.assign(
     { inventory: [], history: [], lifetime: 0, lifetimeWeight: 0.0, canFishDate: 0 },
     customData.get('gofishgame')
   );
+
+  if (player.canFishDate > 0 && player.canFishDate < 1670887583316) {
+    migrate(player);
+  }
+
+  return player;
+}
+
+function migrate(player: Player) {
+  const reset = ['🗡️', '🦂', '🦭', '🐧', '🧭', '👑', '🧜‍♀️'];
+
+  player.history = player.history.filter(
+    record => !reset.includes(record.fish)
+  );
+
+  player.inventory = player.inventory.filter(
+    fish => !reset.includes(fish)
+  );
+
+  player.canFishDate = Date.now();
 }
